df1ba600d58b9f428fa20f1b824c77f9734a9e93
Aleksei Dmitrievstev <93659834+admitrievtsev@users.noreply.github.com>
2024-04-05 18:28:48 +0300

    add: some new Analyser class fields / comments on methods

--- src/analyser.rs
+++ src/analyser.rs
@@ -1,13 +1,23 @@
 use std::collections::HashMap;
+use std::vec::Vec;
 
 pub struct Analyser {
+    f_in: &str,
+    f_out: &str,
+    dict: Vec<&str>,
     //TODO
 }
 impl Analyser {
-    fn make_dict(hm: HashMap<i32, i32>){
-        //TODO
+    fn set(file_in: &str, file_out: &str){
+        self.f_in = file_in;
+        self.f_out = file_out;
     }
-    fn deduplication(array: &mut [i32; 0]){
-        //TODO
+    fn make_dict(hm: HashMap<i32, i32>){
+        //Чтение из f_in
+        //Перебираем файл, делаем набор частых чанков, пишем их в dict
+    }
+    fn deduplication(array: &mut [&str; 0]){
+        //Ищем чанки из dict, проводим дедупликацию, возвращаем массив id и массив чанков 
+        //Запись в f_out
     }
 }
