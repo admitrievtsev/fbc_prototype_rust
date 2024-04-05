2a7272123ef3ee2a325e73ad8ecf5df8a7d96c3f
Aleksei Dmitrievstev <93659834+admitrievtsev@users.noreply.github.com>
2024-04-05 19:37:31 +0300

    add: tester.rs

---
+++ src/tester.rs
@@ -0,0 +1,21 @@
+pub struct Tester{
+  f_in: &str,
+  f_out: &str,
+}
+
+impl Tester{
+    fn set(file_in: &str, file_out: &str){
+        self.f_in = file_in;
+        self.f_out = file_out;
+    } 
+    fn test_size() -> bool{
+        //Вычисляем размер исходного файла и рамер задедуплицированного и сравниваем их
+        //Debuglog
+        //Возвращаем результат сравнения
+    }
+    fn test_content() -> bool{
+        //Пересобираем задедуплицированный файлик и сравниваем с исходным
+        //Debuglog  
+        //Возвращаем результат сравнения
+    }
+}
